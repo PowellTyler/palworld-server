1568075e23ad41a2bfd5735ffd702923f251466a
mayfry <tylernpowell@hotmail.com>
2024-02-17 18:36:46 -0500

    Fix config

--- palserver/config.py
+++ palserver/config.py
@@ -3,6 +3,15 @@ import configparser
 
 __all__ = ['config']
 
+
+def _convert_to_dict(config):
+    d = {}
+    for section in config.sections():
+        d[section] = {}
+        for key, val in config.items(section):
+            d[section][key] = val
+    return d
+
 # TODO: This could be generalized so that config.ini is a set of instructions on
 #       what a config is suppose to be
 #       Examples:
@@ -10,10 +19,11 @@ __all__ = ['config']
 #           b = integer(default=2, min=1, max=999)
 #           c = boolean(default=False)
 #           d = string_list(default=[])
-config = configparser.ConfigParser()
-config.read('/var/lib/palserver/config/config.ini')
-config['env'] = {
-    'root': '/var/lib/palserver',
-    'module': os.path.dirname(os.path.abspath(__file__))
-}
+
+_config = configparser.ConfigParser()
+_config.read('/var/lib/palserver/config/config.ini')
+
+config = _convert_to_dict(_config)
+config['root'] = '/var/lib/palserver'
+config['module'] = os.path.dirname(os.path.abspath(__file__))
 config['app']['auto_update'] = config['app']['auto_update'].lower() == 'false'
