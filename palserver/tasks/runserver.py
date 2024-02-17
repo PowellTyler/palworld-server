97d0c0cfd164a4db871c59f835470a3540868e79
mayfry <tylernpowell@hotmail.com>
2024-02-16 22:31:46 -0500

    Add logging around server run task

--- palserver/tasks/runserver.py
+++ palserver/tasks/runserver.py
@@ -1,4 +1,5 @@
 import subprocess
+from log import log
 from tasks.task import Task
 from config import config
 
@@ -6,5 +7,8 @@ from config import config
 class RunServerTask(Task):
 
     def handler(self):
-        command = f'{config["server"]["game_root"]}/{config["server"]["command_path"]}'
-        subprocess.run([command, '-useperfthreads', '-NoAsyncLoadingThread', '-UseMultithreadForDS'])
+        log.info('event=run_server_task event_details=about_to_start')
+        command = f'{config["server"]["game_root"]}/{config["server"]["command_path"]} -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS'
+        run_server = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        out, err = run_server.communicate()
+        log.info(f'event=run_server_task event_details=exited stdout={out} stderr={err}')
