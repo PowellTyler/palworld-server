fbe4ff33fda1c6ed84f01ddea66c46e692058162
mayfry <tylernpowell@hotmail.com>
2024-02-18 01:39:56 -0500

    Generate service file from podman

--- palserver/tasks/runserver.py
+++ palserver/tasks/runserver.py
@@ -7,6 +7,8 @@ from config import config
 class RunServerTask(Task):
 
     def handler(self):
+        # TODO: It may be good to save the PID in a common file that we can
+        #       look at to make sure a server instance isn't already running.
         run_server_path = f'{config["server"]["game_root"]}/{config["server"]["command_path"]}'
         command = f'su - steam -c "{run_server_path} -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"'
         log.info(f'event=run_server_task event_details=about_to_start command={command}')
