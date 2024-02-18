47a56e3b192fded074b2abbafc9beffcd57b18e0
mayfry <tylernpowell@hotmail.com>
2024-02-18 01:16:42 -0500

    Remove config.ini from install step

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@ ${PYTHON} -m pip install -r /usr/lib/${APP_NAME}/requirements.txt
 
 install /usr/lib/${APP_NAME}/package/ARRCON /usr/bin/ARRCON
 install -d /etc/${APP_NAME}/
-install /usr/lib/${APP_NAME}/package/config.ini /etc/${APP_NAME}/config.ini
 mkdir -p /var/lib/${APP_NAME}/server/Pal/Saved/Config/LinuxServer/
 touch /var/lib/${APP_NAME}/server/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
 
